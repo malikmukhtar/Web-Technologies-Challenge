02245f17edeeba53842834bb599ffabbe6685c88
Malik <malikmukhtar63@gmail.com>
2020-12-30 16:35:09 +0100

    changed api address

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let button = document.getElementById('button');
 
 const fetchData = async () => {
-  let url = 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3';
+  let url =
+    'https://restcountries.eu/rest/v2/all?fields=name;capital;currencies';
   let data = await fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      let output = '<h2>Api Results - Food Recipe</h2>';
-      data.results.map((x) => {
+      let output = '<h2>Locations</h2>';
+      data.map((x) => {
         output += `
               <div class='card'>
-              <h6>${x.title}</h6>
-              <span>${x.ingredients}</span>
-              <a href="${x.href}"></a>
+              <h3>${x.name}</h3>  
+              <h5>${x.capital}</h5>
               </div>
               `;
       });
